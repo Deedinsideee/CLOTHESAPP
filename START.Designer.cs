45041b4cc8ac963d978a2a941bcdfe37c474092a
Deedinsideee <81362238+Deedinsideee@users.noreply.github.com>
2022-02-14 02:10:59 +0300

    <Required>

--- START.Designer.cs
+++ START.Designer.cs
@@ -37,30 +37,34 @@ namespace CLOTHESAPP
             this.bodyTableAdapter = new CLOTHESAPP.DataSet1TableAdapters.bodyTableAdapter();
             this.tableAdapterManager = new CLOTHESAPP.DataSet1TableAdapters.TableAdapterManager();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.fbd = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bodyBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(139, 115);
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(277, 142);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(208, 96);
+            this.button1.Size = new System.Drawing.Size(290, 147);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "подобрать лук";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(483, 115);
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(658, 142);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(208, 96);
+            this.button2.Size = new System.Drawing.Size(313, 147);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "указать что есть что ";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // bodyBindingSource
             // 
@@ -90,20 +94,52 @@ namespace CLOTHESAPP
             this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(1750, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(162, 86);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "на главную";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Trebuchet MS", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(1667, 698);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(259, 84);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "указать пути для шмоток";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // START
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1492, 719);
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(1924, 777);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "START";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)(this.bodyBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             this.ResumeLayout(false);
 
+        }
+
+        private void Button1_Click(object sender, System.EventArgs e)
+        {
+            throw new System.NotImplementedException();
         }
 
         #endregion
@@ -115,6 +151,9 @@ namespace CLOTHESAPP
         private DataSet1TableAdapters.bodyTableAdapter bodyTableAdapter;
         private DataSet1TableAdapters.TableAdapterManager tableAdapterManager;
         private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
